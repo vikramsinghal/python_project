f8887a3de532c408a0de871913c1427850b66cd4
Vikram Singhal <thevikramsinghal@gmail.com>
2019-10-20 21:30:55 -0600

    Started Class Chapter

--- python_work/python_function/store_functions_in_modules.py
+++ python_work/python_function/store_functions_in_modules.py
@@ -1,4 +1,9 @@
-def pizza(size, *toppings):
-    print(f"\nYour {size}-inch pizza is coming with the following toppings:")
+def make_pizza(size, *toppings):
+    print(f"Making a {size}-inch pizza with the following toppings:")
     for topping in toppings:
-        print("f-{topping}")
+        print(f"-{topping}")
+
+import make_pizza
+pizza.make_pizza(16, "peporini")
+
+#For some reason this file isn't working
