340a8d40ec5cd1100636a48eff0d4cfd78a754a4
Vikram Singhal <thevikramsinghal@gmail.com>
2019-10-21 21:54:29 -0600

    practice on classes

--- python_work/python_classes/draft.py
+++ python_work/python_classes/draft.py
@@ -1,18 +1,24 @@
-class Dog:
-    def __init__(self, name, age):
+class Dog(object):
+    """docstring for Dog."""
+
+    def __init__(self, name, age, gender, breed):
         self.name = name
         self.age = age
+        self.gender = gender
+        self.breed = breed
 
-    def sit(self):
-        print(f"{self.name} is now sitting.")
+    def what_breed(self):
+        print(f"{self.name} is a {self.breed}.")
+    def what_name(self):
+        print(f"My dog's name is {self.name}.")
+    def what_gender(self):
+        print(f"{self.name} is a {self.gender}.")
+    def what_age(self):
+        print(f"{self.name} is {self.age} years old.")
 
-    def roll_over(self):
-        print(f"{self.name} rolled over!")
+my_dog = Dog("Kate", 6, "Female", "Corgi")
 
-my_dog = Dog('Willie', 6)
-
-print(f"My dog's name is {my_dog.name}.")
-print(f"My dog is {my_dog.age} years old.")
-
-my_dog.sit()
-my_dog.roll_over()
+my_dog.what_breed()
+my_dog.what_name()
+my_dog.what_gender()
+my_dog.what_age()
