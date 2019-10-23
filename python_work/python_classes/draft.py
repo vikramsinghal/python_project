db03967abc37105dc20869d98cb489142127e302
Vikram Singhal <thevikramsinghal@gmail.com>
2019-10-22 22:46:20 -0600

    practice_class

--- python_work/python_classes/draft.py
+++ python_work/python_classes/draft.py
@@ -1,24 +1,18 @@
-class Dog(object):
+class Dog():
     """docstring for Dog."""
 
-    def __init__(self, name, age, gender, breed):
+    def __init__(self, name, age, breed):
         self.name = name
         self.age = age
-        self.gender = gender
         self.breed = breed
 
-    def what_breed(self):
-        print(f"{self.name} is a {self.breed}.")
     def what_name(self):
         print(f"My dog's name is {self.name}.")
-    def what_gender(self):
-        print(f"{self.name} is a {self.gender}.")
     def what_age(self):
         print(f"{self.name} is {self.age} years old.")
-
-my_dog = Dog("Kate", 6, "Female", "Corgi")
-
-my_dog.what_breed()
+    def what_breed(self):
+        print(f"{self.name} is {self.breed}.")
+my_dog = Dog("Willie", 6, "mix")
 my_dog.what_name()
-my_dog.what_gender()
 my_dog.what_age()
+my_dog.what_breed()
