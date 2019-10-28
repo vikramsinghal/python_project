4837dbcca4c7b2a41cbeb313ce9bf3b84da9d9d0
Vikram Singhal <thevikramsinghal@gmail.com>
2019-10-27 22:43:10 -0600

    random practice

--- python_work/draft/practice.py
+++ python_work/draft/practice.py
@@ -1,48 +1,25 @@
-<<<<<<< HEAD
-person = {
-    'first_name':'Vikram',
-    'last_name':'Singhal',
-    'City':'Boulder',
-}
+class person():
+    """docstring for ."""
 
-person_info = person
-print(f"First name is: {person_info['first_name']}")
-print(f"Last name is: {person_info['last_name']}")
-print(f"City is: {person_info['City']}")
+    def __init__(self, name, age, city):
+        self.name = name
+        self.age = age
+        self.city = city
 
-server = {
-    '10.1.11.139': 'qadenweb73',
-    '10.1.11.140': 'qadenweb72',
-}
-enter_ip = input("Enter IP here: ")
-for ip_address, web_server in server.items():
-    web_server = enter_ip
-    if enter_ip in server:
-        print(f"{enter_ip} belongs to {server[web_server]}")
-        break
-    else:
-        print("Please enter a valid IP.")
-        break
+    def get_information(self):
+        print(f"See below for information about {self.name}")
 
+    def get_name(self):
+        print(f"First name is {self.name}")
 
+    def get_age(self):
+        print(f"{self.name} is {self.age} years old.")
 
-favorite_languages = {
-    'jen': 'python',
-    'sarah': 'c',
-    'edward': 'ruby',
-    'phil': 'python',
-}
-friends = ['phil', 'sarah']
-for name in favorite_languages.keys():
-    print(f"Hi, {name.title()}")
+    def get_city(self):
+        print(f"{self.name} lives in {self.city}.")
 
-    if name in friends:
-        language = favorite_languages[name].title()
-        print(f"{name.title()}, I see you like {language}!")
-=======
-num1 = "36"
-num2 = "5"
-total = (num1 + num2)
-total2 = (num2*5)
-print (total2)
->>>>>>> vikram_macbook
+information = person("Matthew Perry", 34, "New York")
+
+information.get_name()
+information.get_age()
+information.get_city()
