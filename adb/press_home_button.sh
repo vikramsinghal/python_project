036895db1390b2a83c98c3fca74680fd3b70f7c0
vikramsinghal <thevikramsinghal@gmail.com>
2019-11-01 14:22:04 -0600

    Update press_home_button.sh

--- adb/press_home_button.sh
+++ adb/press_home_button.sh
@@ -1,2 +1,6 @@
-adb shell input keyevent 3 #HomeButton
-com.android.phone
+press_home_button=$(adb shell input keyevent 3) #HomeButton
+echo "Home Button has been pressed."
+
+#Launch Google Keep
+launch_google_keep=$(adb shell am start -n com.google.android.keep)
+echo "Google Keep should be up and running on the screen"
